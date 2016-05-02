fe0eeb661f2688b63b8f5fcb087c73355545ad07
matteojoliveau <matteojoliveau@outlook.com>
2016-05-02 16:42:20 +0200

    implemented our own PyDrive libraries, started fixing download memory problem (not yet usable, only downloads PNGs)

--- tuxdrive.py
+++ tuxdrive.py
@@ -21,17 +21,20 @@ gauth.LocalWebserverAuth()
 #list all files in the cloud storage
 drive = GoogleDrive(gauth)
 
+cont = 0
 file_list = drive.ListFile({'q': "'root' in parents and trashed = false"}).GetList()
 for file1 in file_list:
     if re.match("^application/vnd.", file1['mimeType']):
         continue
-    file_id = file1['id']
-    file_name = file1['title']
-    #print(file_id)
-    downloaded_file = drive.CreateFile({'id': file_id})
-    downloaded_file.GetContentFile(drive_dir + "/" + file_name)
-    #print("File Name:", file1['title'])
-    #print("MimeType:", file1['mimeType'])
-    #print()
+    if re.match("^image/png", file1['mimeType']):
+        cont += 1
+        file_id = file1['id']
+        file_name = file1['title']
+        #print(file_id)
+        downloaded_file = drive.CreateFile({'id': file_id})
+        downloaded_file.GetContentFile(drive_dir + "/" + file_name)
+        #print("File Name:", file1['title'])
+        #print("MimeType:", file1['mimeType'])
+        #print()
     
-    
+print("File downloaded:", cont)    
